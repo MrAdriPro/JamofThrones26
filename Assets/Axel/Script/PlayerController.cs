36dccdcab82934c81371b697fefe89557c3fb08d
AlvaroTorresRuiz <xlightez2003@gmail.com>
2026-03-25 14:45:16 +0100

    animaciones metidas falta programar

--- Assets/Axel/Script/PlayerController.cs
+++ Assets/Axel/Script/PlayerController.cs
@@ -38,11 +38,7 @@ public class PlayerController : MonoBehaviour
     public bool _aguantandoLaPuerta;
     public float stamina = 100;
 
-    [Header("Cambio de Modelo")]
-    [SerializeField] GameObject playerModel;
-    [SerializeField] Sprite[] sprites;
-    [SerializeField] Animator[] animatorList;
-    public int currentSpriteIndex = 0;
+   
     #endregion
 
 
@@ -195,20 +191,7 @@ public class PlayerController : MonoBehaviour
         else stamina += Time.deltaTime;
     }
 
-    public void SwapModel()
-    {
-        if (currentSpriteIndex < sprites.Length - 1)
-        {
-            currentSpriteIndex += 1;
-
-            // Adquiere 
-            SpriteRenderer spriteRenderer = playerModel.GetComponent<SpriteRenderer>();
-            //Animator animator = playerModel.GetComponent<Animator>();
-
-            spriteRenderer.sprite = sprites[currentSpriteIndex];
-            //animator = animatorList[currentSpriteIndex];
-        }
-    }
+    
 
     #endregion
 }
