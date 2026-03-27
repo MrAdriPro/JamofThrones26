a8d3a2f55521966ea97bad7853b5aadcaec82f6e
XlightEZ <xlightez2003@gmail.com>
2026-03-27 03:51:53 +0100

    torres y tal

--- Assets/Sergio/Scripts/ShootingTower.cs
+++ Assets/Sergio/Scripts/ShootingTower.cs
@@ -5,49 +5,47 @@ using System.Linq;
 public class ShootingTower : MonoBehaviour
 {
     [SerializeField] public List<Transform> enemiesInRange = new List<Transform>();
-    
+
     [Tooltip("Index 0 = base model, index 1 = upgrade 1, etc. All must be children of this GameObject.")]
     public GameObject[] towerModels;
     [SerializeField] RandoSoundEffecs _randomSoundEffects;
     public Transform target;
+
     [Header("Atributos de la Torre")]
     public float fireRate = 1f;
     private float fireCountdown = 0f;
     public Transform firePoint;
     public GameObject bulletPrefab;
+
+    private Animator _currentAnimator;
+    private int _lastLevel = -1;
+
     void Start()
     {
+        SetupTowerModels();
         UpdateTowerModel();
     }
-    public void ApplyUpgradeLevel(int level)
+
+    void SetupTowerModels()
     {
-        if (towerModels == null || towerModels.Length == 0)
+        Transform body = transform.Find("Body");
+        if (body != null)
         {
-            Debug.LogWarning($"[ShootingTower] No towerModels assigned on {gameObject.name}.");
-            return;
-        }
-
-        int clamped = Mathf.Clamp(level, 0, towerModels.Length - 1);
-
-        for (int i = 0; i < towerModels.Length; i++)
-        {
-            if (towerModels[i] != null)
-                towerModels[i].SetActive(i == clamped);
+            towerModels = new GameObject[body.childCount];
+            for (int i = 0; i < body.childCount; i++)
+            {
+                towerModels[i] = body.GetChild(i).gameObject;
+            }
         }
     }
+
     void Update()
     {
-
-        Transform body = transform.Find("Body");
-    
-        towerModels = new GameObject[body.childCount];
-    
-        for (int i = 0; i < body.childCount; i++)
+        if (ShopManager.shopInstance.tekLevel != _lastLevel)
         {
-            towerModels[i] = body.GetChild(i).gameObject;
-        } 
+            UpdateTowerModel();
+        }
 
-        UpdateTowerModel();
         enemiesInRange.RemoveAll(e => e == null);
 
         enemiesInRange = enemiesInRange
@@ -68,6 +66,7 @@ public class ShootingTower : MonoBehaviour
 
         fireCountdown -= Time.deltaTime;
     }
+
     void LockOnTarget()
     {
         Vector3 dir = target.position - transform.position;
@@ -75,38 +74,47 @@ public class ShootingTower : MonoBehaviour
         Vector3 rotation = lookRotation.eulerAngles;
         transform.rotation = Quaternion.Euler(0f, rotation.y, 0f);
     }
+
     void Shoot()
     {
+        if (_currentAnimator != null)
+        {
+            _currentAnimator.SetTrigger("Shoot");
+        }
+
         GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
         _randomSoundEffects.PlayRandomAttackClip();
+
         BulletController bulletController = bullet.GetComponent<BulletController>();
-        bulletController.bulletTarget = target;
+        if (bulletController != null)
+            bulletController.bulletTarget = target;
     }
+
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Enemy")) enemiesInRange.Add(other.transform);
     }
+
     void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Enemy")) enemiesInRange.Remove(other.transform);
     }
+
     void UpdateTowerModel()
     {
-        int level = ShopManager.shopInstance.tekLevel;
+        _lastLevel = ShopManager.shopInstance.tekLevel;
+
+        if (towerModels == null || towerModels.Length == 0) SetupTowerModels();
+
         for (int i = 0; i < towerModels.Length; i++)
         {
-            towerModels[i].SetActive(i == level);
+            bool isActive = (i == _lastLevel);
+            towerModels[i].SetActive(isActive);
+
+            if (isActive)
+            {
+                _currentAnimator = towerModels[i].GetComponent<Animator>();
+            }
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
